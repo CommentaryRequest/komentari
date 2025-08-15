12095a52e80eda03e909c088b35d1bb9d9e0cc7e
CommentaryRequest <tele.didar.lol@gmail.com>
2025-08-15 22:58:20 +0500

    1.14 add auto mode

---
+++ jpchk.py
@@ -0,0 +1,12 @@
+import re
+
+def is_japan(text):
+    # Including both full-width and half-width katakana
+    jp_chars = re.findall(r'[\u3040-\u309F\u30A0-\u30FF\uFF00-\uFFEF\u4E00-\u9FFF\uFF61-\uFF9F]', text)
+    latin_chars = re.findall(r'[A-Za-z]', text)
+
+    if not jp_chars:
+        return False
+
+    return len(latin_chars) < len(jp_chars) * 0.2  # still allow <20% romaji
+
