05578360a05102adf0965c7938f7e19fb35b1ad3
CommentaryRequest <tele.didar.lol@gmail.com>
2025-08-18 12:54:55 +0500

    1.14.2 handle json decode error

--- posts.py
+++ posts.py
@@ -5,10 +5,13 @@ import urllib
 def get_posts(query, auth, page, headers):
     while True:
         try:
-            return requests.get(f"{get_booru_url()}/posts.json?tags={query}&{str(auth)}&page={page}", timeout=10, headers=headers).json()
+            response = requests.get(f"{get_booru_url()}/posts.json?tags={query}&{str(auth)}&page={page}", timeout=10, headers=headers)
+            return response.json()
         except (
             urllib.error.URLError,
             requests.exceptions.ReadTimeout,
             requests.exceptions.ConnectionError
         ) as exc:
             print(f"Failed to fetch page because of {exc}")
+        except requests.exceptions.JSONDecodeError:
+            print(f"Server returned non-JSON response: {response.text}")
