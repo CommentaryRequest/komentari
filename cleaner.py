aecce4faf2b40407906891495bfac20a4a06287c
CommentaryRequest <tele.didar.lol@gmail.com>
2025-08-19 22:24:48 +0500

    forgot to include cleaner

---
+++ cleaner.py
@@ -0,0 +1,21 @@
+import re
+
+BLOAT = [
+    "pixiv",
+    "fanbox",
+    "skeb",
+    "fantia",
+    "rkgk",
+    "twitter",
+    "misskey"
+]
+
+def remove_hashtags(text):
+    return re.sub(r'"#.*?":\[\S+?\]', "", text)
+
+def remove_urls(text):
+    return re.sub(r"https?://\S+", "", text)
+
+def remove_bloat(text):
+    pattern = re.compile("|".join(re.escape(word) for word in BLOAT), re.IGNORECASE)
+    return pattern.sub("", text)
