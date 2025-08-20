48871fa12c95b96339fa1cf845aa2161baf6f086
CommentaryRequest <tele.didar.lol@gmail.com>
2025-08-20 14:16:50 +0500

    1.14.11 remove twitter links

--- cleaner.py
+++ cleaner.py
@@ -16,6 +16,9 @@ def remove_hashtags(text):
 def remove_urls(text):
     return re.sub(r"https?://\S+", "", text)
 
+def remove_twitter_links(text):
+    return re.sub(r'\[b\]"twitter\/\S+":\[https?:\/\/\S+\]\[/b\]', "", text)
+
 def remove_bloat(text):
     pattern = re.compile("|".join(re.escape(word) for word in BLOAT), re.IGNORECASE)
     return pattern.sub("", text)
