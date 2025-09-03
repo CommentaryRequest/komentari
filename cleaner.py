801fad88b915c64fc8f408f16310097571945ae9
CommentaryRequest <tele.didar.lol@gmail.com>
2025-09-03 22:27:23 +0500

    1.15.1 smarter url cleaner + auto emoji tag

--- cleaner.py
+++ cleaner.py
@@ -14,7 +14,8 @@ def remove_hashtags(text):
     return re.sub(r'"#.*?":\[\S+?\]', "", text)
 
 def remove_urls(text):
-    return re.sub(r"https?://\S+", "", text)
+    no_dtext_urls = re.sub(r"<https?://\S+>", "", text)
+    return re.sub(f"https?://\S+", "", no_dtext_urls)
 
 def remove_twitter_links(text):
     return re.sub(r'\[b\]"twitter\/\S+":\[https?:\/\/\S+\]\[/b\]', "", text)
