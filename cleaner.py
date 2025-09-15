d89e08c7c786085318d64314d67f0e6b0400ca71
CommentaryRequest <tele.didar.lol@gmail.com>
2025-09-15 17:42:37 +0500

    1.16.2 strip pixiv user links

--- cleaner.py
+++ cleaner.py
@@ -17,10 +17,11 @@ def remove_hashtags(text):
     return re.sub(r'"#.*?":\[\S+?\]', "", text)
 
 def remove_urls(text):
-    no_dtext_urls = re.sub(r"<https?://\S+>", "", text)
-    no_twitter_links = re.sub(r'\[b\]"twitter\/\S+":\[https?:\/\/\S+\]\[\/b\]', "", no_dtext_urls)
-    no_at_mentions = re.sub(r'"@\S+":\[https?://\S+\]', "", no_twitter_links)
-    return re.sub(f"https?://\S+", "", no_at_mentions)
+    text = re.sub(r"<https?://\S+>", "", text)
+    text = re.sub(r'\[b\]"twitter\/\S+":\[https?:\/\/\S+\]\[\/b\]', "", text)
+    text = re.sub(r'\[b\]"user\/\S+":\[https:\/\/\S+\] "»":\[\/\S+\]\[\/b\]', "", text)
+    text = re.sub(r'"@\S+":\[https?://\S+\]', "", text)
+    return re.sub(f"https?://\S+", "", text)
 
 def remove_bloat(text):
     pattern = re.compile(r"(?<![A-Za-z0-9])(?:" + "|".join(map(re.escape, BLOAT)) + r")(?![A-Za-z0-9])", re.IGNORECASE)
