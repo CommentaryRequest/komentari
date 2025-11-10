351a1f3ce2f24b57065506947172e82a4b081450
CommentaryRequest <tele.didar.lol@gmail.com>
2025-11-10 15:27:07 +0500

    1.18 fix commentaries with empty chars and add put in favgroup unrecognized

--- komentari.py
+++ komentari.py
@@ -24,7 +24,7 @@ import cleaner
 import cliargs
 import recog
 
-__version__ = "1.17"
+__version__ = "1.18"
 
 USERAGENT = f"Komentari/{__version__} by user #1054326"
 
@@ -208,33 +208,39 @@ def main():
                             else:
                                 clean_commentary = commentary.og_title + " " + commentary.og_description
 
-                                clean_commentary = cleaner.remove_hashtags(clean_commentary)
-                                manual_input = False
+                                clean_commentary = cleaner.remove_invisible_chars(clean_commentary)
                                 if len(clean_commentary.strip()) == 0:
-                                    # The commentary only contained hashtags
-                                    parsed_input = "hashtag-only_commentary"
+                                    print("Only invisible characters")
+                                    parsed_input = parser.NONPERMANENT_SKIP
+                                    manual_input = semi_auto
                                 else:
-                                    clean_commentary = cleaner.remove_urls(clean_commentary)
-                                    is_emoji = emjchk.is_emoji(clean_commentary)
-                                    if is_emoji:
-                                        parsed_input = "symbol-only_commentary"
+                                    clean_commentary = cleaner.remove_hashtags(clean_commentary)
+                                    manual_input = False
+                                    if len(clean_commentary.strip()) == 0:
+                                        # The commentary only contained hashtags
+                                        parsed_input = "hashtag-only_commentary"
                                     else:
-                                        clean_commentary = cleaner.remove_bloat(clean_commentary)
-                                        if not quiet:
-                                            print(f"Clean commentary = {clean_commentary}")
-                                        is_japan = jpchk.is_japan(clean_commentary)
-                                        is_korea = kkchk.is_korea(clean_commentary)
-                                        if is_korea:
-                                            parsed_input = "commentary_request korean_commentary"
-                                        elif is_japan:
-                                            parsed_input = "commentary_request"
+                                        clean_commentary = cleaner.remove_urls(clean_commentary)
+                                        is_emoji = emjchk.is_emoji(clean_commentary)
+                                        if is_emoji:
+                                            parsed_input = "symbol-only_commentary"
                                         else:
-                                            confidence = check_en(clean_commentary, post_id, en_log)
-                                            if confidence >= 0.9:
-                                                parsed_input = "commentary english_commentary"
+                                            clean_commentary = cleaner.remove_bloat(clean_commentary)
+                                            if not quiet:
+                                                print(f"Clean commentary = {clean_commentary}")
+                                            is_japan = jpchk.is_japan(clean_commentary)
+                                            is_korea = kkchk.is_korea(clean_commentary)
+                                            if is_korea:
+                                                parsed_input = "commentary_request korean_commentary"
+                                            elif is_japan:
+                                                parsed_input = "commentary_request"
                                             else:
-                                                parsed_input = parser.NONPERMANENT_SKIP
-                                                manual_input = semi_auto
+                                                confidence = check_en(clean_commentary, post_id, en_log)
+                                                if confidence >= 0.9:
+                                                    parsed_input = "commentary english_commentary"
+                                                else:
+                                                    parsed_input = f"favgroup:{settings.UNRECOG_FAVGROUP}"
+                                                    manual_input = semi_auto
 
                         if manual_input:
                             print("(h for help)")
