74e1172fb9b2166e9ba2c40e9f64293ea463cd21
CommentaryRequest <tele.didar.lol@gmail.com>
2025-11-30 12:08:42 +0500

    1.18.8 more symbol support

--- emjchk.py
+++ emjchk.py
@@ -1,8 +1,13 @@
 import emoji
+import string
+
+symbols_emoji = list(emoji.EMOJI_DATA)
+symbols_punct = list(string.punctuation)
+symbols = symbols_emoji + symbols_punct
 
 def is_emoji(text):
     text = text.strip().replace("\uFE0F", "") # variatoin selector
     if not text:
         return False
 
-    return all(ch in emoji.EMOJI_DATA for ch in text)
+    return all(ch in symbols for ch in text)
