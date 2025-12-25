3e225d309f65cc2e97ef8e91b8f43bc45ad710cb
CommentaryRequest <tele.didar.lol@gmail.com>
2025-12-25 22:11:43 +0500

    1.22 add commentary downloader

--- numchk.py
+++ numchk.py
@@ -2,4 +2,4 @@ import re
 import emjchk
 
 def is_numbers(text):
-    return all(char.isdigit() or char in emjchk.symbols_punct for char in text)
+    return all(char.isdigit() or char in emjchk.symbols_punct + [" "] for char in text)
