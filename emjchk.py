caa6256a8533acdcc9b37e32117d7e28984c959b
CommentaryRequest <tele.didar.lol@gmail.com>
2025-12-25 18:08:30 +0500

    1.21.3 handle braille

--- emjchk.py
+++ emjchk.py
@@ -1,5 +1,6 @@
 import emoji
 import string
+import re
 
 symbols_emoji = list(emoji.EMOJI_DATA)
 symbols_punct = list(string.punctuation)
@@ -9,5 +10,9 @@ def is_emoji(text):
     text = text.strip().replace("\uFE0F", "") # variatoin selector
     if not text:
         return False
+
+    text = re.sub(r"[\u2800-\u283F\u2840-\u28FF]", "", text).strip()
+    if len(text) == 0:
+        return True
 
     return all(ch in symbols for ch in text)
