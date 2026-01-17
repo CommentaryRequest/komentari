ac615bc0788dc894d18473731c6777054b2e13e5
CommentaryRequest <tele.didar.lol@gmail.com>
2026-01-17 21:56:26 +0500

    1.24.14 better symbol recognition

--- emjchk.py
+++ emjchk.py
@@ -1,10 +1,22 @@
 import emoji
 import string
 import re
+import unicodedata
 
 symbols_emoji = list(emoji.EMOJI_DATA)
 symbols_punct = list(string.punctuation)
-symbols = symbols_emoji + symbols_punct + [" "]
+symbols = symbols_emoji + symbols_punct + [" "] # this one is here just in case. idk.
+
+symbol_categories = ["Po", "Pc", "Pd", "Ps", "Pe", "Pi", "Pf", "Sm", "Sc", "Sk", "So"]
+
+def test_category(ch):
+    for cat in symbol_categories:
+        if unicodedata.category(ch) == cat:
+            return True
+    return False
+
+def is_symbol(ch):
+    return ch in symbols or test_category(ch)
 
 def is_emoji(text):
     text = text.strip().replace("\uFE0F", "") # variatoin selector
@@ -16,4 +28,4 @@ def is_emoji(text):
     if len(text) == 0:
         return True
 
-    return all(ch in symbols for ch in text)
+    return all(is_symbol(ch) for ch in text)
