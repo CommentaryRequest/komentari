4737b956cc2b4d7da4dd9d18f3aa613b9beeb193
CommentaryRequest <tele.didar.lol@gmail.com>
2026-07-01 14:49:07 +0500

    1.31.3 handle unsuccessful post response (downbooru etc.)

--- posts.py
+++ posts.py
@@ -9,7 +9,10 @@ def get_posts(query, auth, page, headers, test_mode):
             posts_url = f"{get_booru_url(test_mode)}/posts.json?tags={query}&{str(auth)}&page={page}"
             dprint(f"Execute request to {posts_url}")
             response = requests.get(posts_url, timeout=10, headers=headers)
-            return response.json(), response.text
+            if "success" in response.json() and not response.json()["success"]:
+                print(f"Unsuccessful response: {response.json()}")
+            else:
+                return response.json(), response.text
         except requests.exceptions.JSONDecodeError:
             print(f"Server returned non-JSON response: {response.text}")
         except (
