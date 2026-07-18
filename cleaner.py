5de737f27086faade389e457b22795d15ba67cd6
CommentaryRequest <tele.didar.lol@gmail.com>
2026-07-18 23:32:58 +0500

    1.31.5 fix cleaner.py broken regex

--- cleaner.py
+++ cleaner.py
@@ -54,7 +54,7 @@ def remove_urls(text):
     text = re.sub(r'\[b\]"user\/\S+":\[https:\/\/\S+\] "»":\[\/\S+\]\[\/b\]', "", text)
     text = re.sub(r'\[b\]pixiv #\d+ "»":\[\S+\]\[\/b\]', "", text)
     text = re.sub(r'"@\S+":\[https?://\S+\]', "", text)
-    return re.sub(f"https?://\S+", "", text)
+    return re.sub(r"https?://\S+", "", text)
 
 def remove_bloat(text):
     pattern = re.compile(r"(?<![A-Za-z0-9])(?:" + "|".join(map(re.escape, BLOAT)) + r")(?![A-Za-z0-9])", re.IGNORECASE)
--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 from authsettings import *
 
-PROGRAM_VERSION = "1.31.4"
+PROGRAM_VERSION = "1.31.5"
 USERAGENT = f"Komentari/{PROGRAM_VERSION} by user #1054326"
 
 DEBUGMODE = False
