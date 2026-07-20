0489477fcf4a26f000f3544d95f891f35300e188
CommentaryRequest <tele.didar.lol@gmail.com>
2026-07-20 23:48:31 +0500

    split main loop

--- komentari.py
+++ komentari.py
@@ -26,6 +26,52 @@ HEADERS = {
     "User-Agent": settings.USERAGENT
 }
 
+def run_offline(offline_posts, tag_script, args, exec_ctx):
+    for i, (post_id, post) in enumerate(offline_posts.items()):
+        ctx = OfflineContext(tag_script, args.offline_output, Commentary(post["og_title"], post["og_description"], post["tl_title"], post["tl_description"]), len(offline_posts), i)
+        post_info = PostInfo(post_id, None, None, None, None, None)
+        exec_ctx.edit_count += processor.process_offline(args, post_info, exec_ctx, ctx)
+    print("No more posts.")
+    exec_ctx.skipped_posts.flush()
+    write_tag_script(args.offline_output, tag_script)
+
+def run_online(args, exec_ctx, net_ctx):
+    page = args.initial_page
+    while True:
+        if args.random or args.same_page:
+            print("Getting more posts...")
+        else:
+            print(f"Now on page {page}")
+        posts, raw_resp = get_posts(args.query, page, net_ctx)
+        if posts == []:
+            print("No more posts.")
+            skipped_posts.flush()
+            print(f"gardened {exec_ctx.edit_count} posts")
+            break
+
+        debug.dprint(f"post response: {posts}")
+
+        for post in posts:
+            # checking posts for unwanted tags
+            check_result, bad_tag = post_check.check_post(post)
+            if check_result == post_check.POST_CHECK_CENTAG:
+                print(f"Contains unwanted tag: {bad_tag}")
+                continue
+            elif check_result == post_check.POST_CHECK_IS_BANNED:
+                if settings.BANNED_FAVGROUP:
+                    print(f"Is banned; adding to favgroup #{settings.BANNED_FAVGROUP}")
+                    add_to_favgroup(settings.BANNED_FAVGROUP, post["id"], net_ctx)
+                else:
+                    print("Is banned; skipping")
+                continue
+
+            # Getting post information
+            dprint(f"Working with post = {json.dumps(post, indent=2)}")
+            dprint(f"Raw response = {raw_resp}")
+            post_info = PostInfo.from_json(post)
+            exec_ctx.edit_count += processor.process_online(args, post_info, exec_ctx, net_ctx)
+        page += 0 if args.random or args.same_page else 1
+
 def main():
     print(f"komentari {settings.PROGRAM_VERSION} is up")
 
@@ -73,54 +119,14 @@ def main():
 
     skipped_posts = skipped.SkippedPosts()
 
-    page = args.initial_page
     exec_context = ExecutionContext(skipped_posts, 0)
     net_context = NetworkContext(auth, HEADERS, args.test_mode)
 
     try:
         if args.offline_file:
-            for i, (post_id, post) in enumerate(offline.items()):
-                ctx = OfflineContext(tag_script, args.offline_output, Commentary(post["og_title"], post["og_description"], post["tl_title"], post["tl_description"]), len(offline), i)
-                post_info = PostInfo(post_id, None, None, None, None, None)
-                exec_context.edit_count += processor.process_offline(args, post_info, exec_context, ctx)
-            print("No more posts.")
-            skipped_posts.flush()
-            write_tag_script(args.offline_output, tag_script)
+            run_offline(offline, tag_script, args, exec_context)
         else:
-            while True:
-                if args.random or args.same_page:
-                    print("Getting more posts...")
-                else:
-                    print(f"Now on page {page}")
-                posts, raw_resp = get_posts(args.query, page, net_context)
-                if posts == []:
-                    print("No more posts.")
-                    skipped_posts.flush()
-                    print(f"gardened {exec_context.edit_count} posts")
-                    break
-
-                debug.dprint(f"post response: {posts}")
-
-                for post in posts:
-                    # checking posts for unwanted tags
-                    check_result, bad_tag = post_check.check_post(post)
-                    if check_result == post_check.POST_CHECK_CENTAG:
-                        print(f"Contains unwanted tag: {bad_tag}")
-                        continue
-                    elif check_result == post_check.POST_CHECK_IS_BANNED:
-                        if settings.BANNED_FAVGROUP:
-                            print(f"Is banned; adding to favgroup #{settings.BANNED_FAVGROUP}")
-                            add_to_favgroup(settings.BANNED_FAVGROUP, post["id"], net_context)
-                        else:
-                            print("Is banned; skipping")
-                        continue
-
-                    # Getting post information
-                    dprint(f"Working with post = {json.dumps(post, indent=2)}")
-                    dprint(f"Raw response = {raw_resp}")
-                    post_info = PostInfo.from_json(post)
-                    exec_context.edit_count += processor.process_online(args, post_info, exec_context, net_context)
-                page += 0 if args.random or args.same_page else 1
+            run_online(args, exec_context, net_context)
     except KeyboardInterrupt:
         skipped_posts.flush()
         if args.offline_file:
