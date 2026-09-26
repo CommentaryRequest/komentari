0f821145b87793d9a2a7809cd2cdf07046298e86
CommentaryRequest <tele.didar.lol@gmail.com>
2026-09-26 23:21:23 +0500

    Fix unseparated dates not requiring 0 for single digit date/month numbers

--- automode.py
+++ automode.py
@@ -25,7 +25,7 @@ UNTITLED_TITLES = [
 # Date regexes match when one day/month component is 01-12, the other 01-31. Leading zero optional.
 
 # Unseparated dates; matches year=2000-2099 to avoid false positives.
-DATE_REGEX_UNSEPARATED = r"(?<![0-9])20[0-9]{2}(((0?[1-9]|[1-2][0-9]|3[0-1])(0?[1-9]|1[0-2]))|((0[1-9]|1[0-2])(0?[1-9]|[1-2][0-9]|3[0-1])))(?![0-9])"
+DATE_REGEX_UNSEPARATED = r"(?<![0-9])20[0-9]{2}(((0[1-9]|[1-2][0-9]|3[0-1])(0[1-9]|1[0-2]))|((0[1-9]|1[0-2])(0[1-9]|[1-2][0-9]|3[0-1])))(?![0-9])"
 
 # Dates separated with '.', '-' or '/'. Matches year=1000-2999. Day optional.
 DATE_REGEX_SEPARATED = r"(?<![0-9])[1-2][0-9]{3}(\.|-|\/)(((((0?[1-9]|[1-2][0-9]|3[0-1]))\1((0?[1-9]|1[0-2])))|(((0?[1-9]|1[0-2]))\1((0?[1-9]|[1-2][0-9]|3[0-1]))))|(0?[1-9]|1[0-2]))(?![0-9])"
